90aab48184a139fb6159393ad8d75c36708ed0f5
daredevil17052004 <anshs052004@gmail.com>
2026-08-17 14:39:32 +0530

    client folder created

---
+++ client/next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  async rewrites(){
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*"
+      }
+    ]
+  },
+  reactCompiler: true,
+};
+
+export default nextConfig;
