c37e04aa91fa1f19b44158c61520d6f0ab8c58fe
daredevil17052004 <anshs052004@gmail.com>
2026-08-25 17:00:08 +0530

    fix for redis

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -1,7 +1,9 @@
 import { Redis } from "ioredis";
 
-export const redisConnection = new Redis({
-    host: "localhost",
-    port: 6379,
-    maxRetriesPerRequest: null,
-})
+export const redisConnection = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+  : new Redis({
+      host: "localhost",
+      port: 6379,
+      maxRetriesPerRequest: null,
+  });
